2dfb8e4a95363f907dfcad2ea2d3ed9e23ad99ea
Natthapol SERMSARAN <natthapolwork@gmail.com>
2026-03-22 04:16:53 +0700

    chore: install Vitest and JSDOM with configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  plugins: [],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
   server: {
     port: 3000,
   },
